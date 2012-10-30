72093fb44a19cbf90a722c2af50a706b3b045954
Steve Smith <sxsmith@book.com>
2012-10-30 13:37:16 -0700

    refactor to dynamic function; expose log methods on window

--- test/r_logger.test.js
+++ test/r_logger.test.js
@@ -3,18 +3,18 @@ test("logger:init", function() {
   delete localStorage.loggerLevel;
   logger.init();
 
-  ok( parseInt(localStorage.loggerLevel) == logger.levels.WARN, "Passed!");
+  ok( parseInt(localStorage.loggerLevel) == logger.levels.WARNING, "Passed!");
 
   logger.init();
-  ok( parseInt(localStorage.loggerLevel) == logger.levels.WARN, "Passed!");
+  ok( parseInt(localStorage.loggerLevel) == logger.levels.WARNING, "Passed!");
 
 });
 
 test("logger:level", function() {
   
   logger.init();
-  ok( parseInt(localStorage.loggerLevel) == logger.levels.WARN, "Passed!");
-  ok( logger.level() == logger.levels.WARN, "Passed!");
+  ok( parseInt(localStorage.loggerLevel) == logger.levels.WARNING, "Passed!");
+  ok( logger.level() == logger.levels.WARNING, "Passed!");
 
   logger.level(logger.levels.ERROR);
   ok( logger.level() == logger.levels.ERROR, "Passed!");
@@ -31,5 +31,16 @@ test("logger:debug", function() {
 
   logger.level(logger.levels.ERROR);
   ok( logger.debug("test failed", 123) == false, "Passed!");
+
+});
+
+test("window:debug", function() {
+  
+  logger.level(logger.levels.DEBUG);
+  ok( debug("bad").search(/\[[0-9\.:]*\] \[DEBUG\].*bad$/) != -1, "Passed!");
+  ok( debug("test failed", 123).search(/\[[0-9\.:]*\] \[DEBUG\].*test failed 123$/) != -1, "Passed!");
+
+  logger.level(logger.levels.NONE);
+  ok( debug("test failed", 123) == false, "Passed!");
 
 });
