6ce3c70c55e42cd68ce370de2ec5470192ba4a70
dristic <dristic@book.com>
2012-11-03 23:20:12 -0700

    More object functions.

--- test/r_object.test.js
+++ test/r_object.test.js
@@ -1,7 +1,6 @@
 test("object:class", function () {
   function Foo() {}
   var obj = new Foo();
-  console.log(obj.class());
   equal(obj.class(), Foo, "Class should equal the instance function.");
 });
 
@@ -12,4 +11,24 @@ test("object:clone", function () {
   var other = obj.clone();
 
   equal(other.foo, obj.foo, "The cloned keys should be equal.");
+});
+
+test("object:define_singleton_method", function () {
+  function Foo() {}
+  var obj = new Foo();
+  obj.define_singleton_method("hello", function () {
+    return "hello";
+  });
+  equal(Foo.hello(), "hello", "The singleton method should be defined.");
+});
+
+test("object:display", function () {
+  var passed = false;
+  port = {
+    write: function () {
+      passed = true;
+    }
+  };
+  new Object().display(port);
+  ok(passed, "The port's write method should be called.");
 });
